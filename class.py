9062eeb7bbaa356100109c7df7bad4616590ab82
michaelkrause19940107 <92281427+michaelkrause19940107@users.noreply.github.com>
2021-10-23 18:09:02 +0900

    abc

--- class.py
+++ class.py
@@ -1,6 +1,6 @@
-class MyClass():
+class michael():
     def __init__(self, message):
         self.value = message
  
-myinstance = MyClass("Hello!")
+myinstance = michael("Hello!")
 print(myinstance.value)
