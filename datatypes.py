9a307506a72fce2fa57656de05f5bbc6366fcd74
michaelkrause19940107 <92281427+michaelkrause19940107@users.noreply.github.com>
2021-10-24 19:09:42 +0900

    practice

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+x=[
+"hello world",
+20,
+20.5,
+1j,
+["apple","banana","cherry"],
+range(6),
+("a","b","c"),
+{"name":"a","age":36},
+{"a","b","c"},
+frozenset({"a","b","c"}),
+True,
+b"Helo",
+bytearray(5),
+memoryview(bytes(5))
+]
+for y in x:
+    print(y)
+
+print(bytes(5))
+
+# x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14=x
+# print(type(x1))
+# print(type(x2))
+# print(type(x3))
+# print(type(x4))
+# print(type(x5))
+# print(type(x6))
+# print(type(x7))
+# print(type(x8))
+# print(type(x9))
+# print(type(x10))
+# print(type(x11))
+# print(type(x12))
+# print(type(x13))
+# print(type(x14))
---
+++ variables.py
@@ -0,0 +1,51 @@
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4
+# x = "Sally"
+# print(x)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# x, y, z=str("ab"), int(3), float(3)
+
+# print(x)
+# print(y)
+# print(z)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# Michael = [int(1), str("math"), "programming"]
+# a,b,c=Michael
+# print(a)
+# # print(b)
+# print(c)
+
+# x = "awesome"
+# y="python is "
+# print("python is " + x)
+# print(y + x)
+
+# x1 = 5
+# x2 = 10
+# print(x1 + x2)
+
+# x = "5 times "
+# y = "John"
+# print(x + y)
+
+x3 = "awesome"
+
+def myfunc():
+    x3 = "fantastic"
+    print("python is " + x3)
+
+myfunc()
+
+print("python is " + x3)
